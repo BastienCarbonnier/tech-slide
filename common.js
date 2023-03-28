8990741680e5935bd210e8844a0551be213ad340
Bastien Carbonnier <bastien.carbonnier@gmail.com>
2023-03-28 03:04:53 +0000

    develop: Add themes selection and loading of presentations by object

--- common.js
+++ common.js
@@ -1,16 +1,42 @@
-let theme = new URLSearchParams(window.location.search).get('theme');
-theme = theme !== undefined ? theme : 'portfolio';
+const themes = [
+  {
+    id: "maplr",
+    label: "Maplr",
+    logo: "../assets/logo-maplr.png",
+  },
+  {
+    id: "intact",
+    label: "Intact Lab",
+    logo: undefined,
+  },
+  {
+    id: "portfolio",
+    label: "Portfolio",
+    logo: undefined,
+  },
+];
 
-const logo_by_theme = {
-  'maplr': '../assets/logo-maplr.png'
-};
+const presentations = [
+  {
+    id: "keycloak",
+    title: "Keycloak présentation",
+    description: "Présentation sur Keycloak et l'authentification en général.",
+  },
+];
 
-let logo = logo_by_theme[theme];
-if (!!logo) {
-  document.write('\
-				<link rel="stylesheet" href="../dist/theme/' + theme + '.css">\
-			');
+function getThemeForId(id) {
+  return themes.find((theme) => theme.id === id);
 }
 
-const href = window.location.href.replace('index.html', '');
-const presentationsUrl = href + 'presentations/';
+const href = window.location.href.replace("index.html", "");
+const presentationsUrl = href + "presentations/";
+
+let theme_id = new URLSearchParams(window.location.search).get("theme");
+theme_id = theme_id !== undefined ? theme_id : "portfolio";
+const theme = getThemeForId(theme_id);
+
+if (!!theme?.logo) {
+  document.write(
+    '<link rel="stylesheet" href="../dist/theme/' + theme.id + '.css">'
+  );
+}
---
+++ presentations.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  let themes_html = "";
+  themes.forEach((theme) => {
+    themes_html +=
+      '<option value="' +
+      theme.id +
+      "-${presentation_id}" +
+      '">' +
+      theme.label +
+      "</option>";
+  });
+
+  // We fetch card template and replace all value we need
+  fetch("card_presentation.html")
+    .then((response) => response.text())
+    .then((card) => {
+      let cards = "";
+      presentations.forEach((presentation) => {
+        let presentation_card = `${card}`;
+        presentation_card = presentation_card
+          .replace("${title}", presentation.title)
+          .replace("${description}", presentation.description)
+          .replace("${themes}", themes_html)
+          .replaceAll("${presentation_id}", presentation.id);
+        cards += presentation_card;
+      });
+      document.getElementById("cards-list").innerHTML = cards;
+
+      const btn_see_presentation = document.querySelector("#see-presentation");
+      const theme_select = document.querySelector("#themes");
+
+      btn_see_presentation.onclick = (event) => {
+        event.preventDefault();
+        const theme_selected_split = theme_select.value.split("-");
+        goToPresentation(theme_selected_split[1], theme_selected_split[0]);
+      };
+    });
+});
+
+function goToPresentation(presentation, theme) {
+  window.location.href =
+    presentationsUrl + presentation + ".html?theme=" + theme;
+}
