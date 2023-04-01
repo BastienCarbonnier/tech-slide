bc99f4b76ac7187c8b7db174e29e9c0320f1a8a9
Bastien Carbonnier <bastien.carbonnier@gmail.com>
2023-04-01 05:09:58 +0000

    WIP

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ const themes = [
   {
     id: "maplr",
     label: "Maplr",
-    logo: "../assets/logo-maplr.png",
+    logo: "./assets/logo-maplr.png",
   },
   {
     id: "intact",
@@ -28,6 +28,32 @@ function getThemeForId(id) {
   return themes.find((theme) => theme.id === id);
 }
 
+let deep_path = '';
+function setActualDeep() {
+  const href = window.location.href;
+  let deep = (href.match(/\//g) || []).length - 2;
+  
+  if (href.includes("tech-slides")) {
+    deep = deep - 1;
+  }
+  let last_char_is_slash = href[href.length - 1] === "/";
+  if (!last_char_is_slash) {
+    let index_interrogation = href.indexOf('?');
+    if (index_interrogation !== -1) {
+      last_char_is_slash = href[index_interrogation-1] === "/";
+    }
+  }
+  if ((last_char_is_slash && deep === 1) || deep === 0) {
+    deep_path = './';
+  } else {
+    if (last_char_is_slash) {
+      deep = deep - 1;
+    }
+    deep_path = '../'.repeat(deep);
+  }
+}
+setActualDeep();
+
 const href = window.location.href.replace("index.html", "");
 const presentationsUrl = href + "presentations/";
 
@@ -35,8 +61,4 @@ let theme_id = new URLSearchParams(window.location.search).get("theme");
 theme_id = theme_id !== undefined ? theme_id : "portfolio";
 const theme = getThemeForId(theme_id);
 
-if (!!theme?.logo) {
-  document.write(
-    '<link rel="stylesheet" href="../dist/theme/' + theme.id + '.css">'
-  );
-}
+document.write('<link rel="stylesheet" href="' + deep_path + 'dist/theme/' + theme.id + '.css">');
